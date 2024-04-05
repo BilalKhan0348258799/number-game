32ad97a7c699683bb68ba282a4813f19a0174a66
BilalKhan0348258799 <muhammadbilalfareed94@gmail.com>
2024-04-05 08:19:08 -0700

    main

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const randomNumber = Math.floor(Math.random() * 10 + 1);
 const answers = await inquirer.prompt([{
     name : "userGuessNumber",
     type : "number",
-    message : "Please guess a number between 1 to 10:"
+    message : "kindly guess a number between 1 to 10:"
 }]);
 if (answers.userGuessNumber ===randomNumber){
     console.log("congtratulation you guess a right number")
